34fa16123eba17937abcc31d61a2a459fd9db65e
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-07-15 17:49:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,8 +33,8 @@ async def link_handler(bot, message):
 
 
 async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
-    params = {'api': API_KEY, 'url': link}
+    url = ' https://bitly.awslink.in/api/?key'
+    params = {'api/?key': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
